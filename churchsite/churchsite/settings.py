b106fe382b27f7ba6c24d9c24dad1673b48de8a2
Nathaniel Cherian <ngcherian2014@gmail.com>
2020-07-17 20:17:35 -0700

    fixed navbar font

--- churchsite/churchsite/settings.py
+++ churchsite/churchsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-3w(ywfp^**w2piwo4=y@y0r@b@t(s(&q)+&eq6owlmknup+o('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
